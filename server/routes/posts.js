cfce758db624b5110dd8749265205d36382ab2ac
Samiksha-Pansare <pansaresamiksha@gmail.com>
2022-10-05 10:09:51 +0530

    Delete Functionality Added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import { getPost, getPosts , createPost, updatePost} from "../controllers/posts.js"
-
-
+import { getPosts, createPost, updatePost,  deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -9,4 +7,7 @@ router.get('/', getPosts);
 router.post('/', createPost);
 // router.get('/:id', getPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+// router.patch('/:id/likePost', likePost);
+
 export default router;
